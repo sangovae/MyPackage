bc0ca905e29d84ffeabf3066aa5361fd99a612cb
Sanne Govaert <sangovae@Boswilg.UGent.be>
2024-02-03 21:25:18 +0100

    testing a new test

--- tests/testthat/test-vespadata.R
+++ tests/testthat/test-vespadata.R
@@ -1,6 +1,8 @@
 test_that("Whether vespa_data works with a single year as input", {
 
-  year = sample(seq(2017,2024))
-  expect(dim(vespa_data(2017))!=0)
+  year = sample(seq(2017,2024), 1)
+  #expect(!is.null(dim(vespa_data(year))))
+
+  expect_no_condition(vespa_data(year))
 
 })
