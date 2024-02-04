a46fc13093b66f3def0d505f4eebf862d7a3b935
Sanne Govaert <sangovae@Boswilg.UGent.be>
2024-02-04 20:48:25 +0100

    Add labels with number of observations

--- R/vespa_chart.R
+++ R/vespa_chart.R
@@ -7,8 +7,9 @@
 #' @return A bar chart of observations per year
 #' @export
 #'
-#' @importFrom ggplot2 ggplot geom_bar aes xlab ylab
+#' @importFrom ggplot2 ggplot geom_bar aes xlab ylab geom_text theme_bw
 #' @importFrom rlang .data
+#' @importFrom dplyr group_by summarise
 #'
 #' @examples
 #' # First, create a tibble with observations with the function vespa_data
@@ -22,10 +23,14 @@
 #' ggtitle("Number of observations of Vespa velutina in Flanders during 2019-2021")}
 
 vespa_chart <- function(data) {
-vespa_barchart <- data %>%
-    ggplot2::ggplot(aes(x=as.factor(.data$year))) +
-    ggplot2::geom_bar()+
-  ggplot2::xlab("Year") + ggplot2::ylab("Observations")
+  vespa_barchart <- data %>%
+    group_by(year) %>%
+    summarise(observations = n()) %>%
+    ggplot2::ggplot(aes(x=.data$year, y = .data$observations)) +
+    ggplot2::geom_bar(position = 'dodge', stat='identity') +
+    geom_text(aes(label=.data$observations), position=position_dodge(width=0.9), vjust=-0.25)+
+    ggplot2::xlab("Year") + ggplot2::ylab("Observations") +
+    ggplot2::theme_bw()
+
   return(vespa_barchart)
 }
-
