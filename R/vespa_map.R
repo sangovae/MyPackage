96ed084124634fd3a0c32fc380ba7e5cda223ab1
Sanne Govaert <sangovae@Boswilg.UGent.be>
2024-02-04 22:38:22 +0100

    add color to manual scale

--- R/vespa_map.R
+++ R/vespa_map.R
@@ -36,7 +36,7 @@ vespa_map <- function(data) {
     ggplot2::scale_x_continuous(limits = c(2.5,6), breaks = seq(2.5, 6, 0.25), expand = c(0, 0)) +
     ggplot2::scale_y_continuous(limits = c(50.5,51.5), breaks = seq(50.5,51.5, 0.25),expand = c(0, 0))+
     ggplot2::xlab("Longitude") + ggplot2::ylab("Latitude") +
-    ggplot2::scale_color_manual(values = rev(c("#710000", "#A20000", "#D60000", "#ED2E40", "#FB6C73", "#FF9CA1", "#FFCACC"))) +
+    ggplot2::scale_color_manual(values = rev(c("#710000", "#9C0000", "#C90000", "#E60026", "#F5505B", "#FE7F85", "#FFA8AB", "#FECFD1"))) +
     ggplot2::theme_bw() +
     ggplot2::theme(legend.position = 'bottom',
                    legend.title = ggplot2::element_blank(),
