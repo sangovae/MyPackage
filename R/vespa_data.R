d6a3d06f652a981d65a93fa743105774fd3bc1ce
Sanne Govaert <sangovae@Boswilg.UGent.be>
2024-02-04 19:12:26 +0100

    update documentation

--- R/vespa_data.R
+++ R/vespa_data.R
@@ -1,10 +1,11 @@
 #' @title Retrieve occurrences from GBIF of Vespa velutina
 #' @description
 #' code{vespa_data} retrieves occurrences from the Global Biodiversity Information Facility (GBIF) for
-#' a single year or year range of Vespa velutina in Flanders.
+#' a single year or year range of \emph{Vespa velutina} in Flanders.
+#' \emph{Vespa velutina} was first observed in 2017 in Flanders.
+#' Therefore, your year range should start at least at 2017 or later.
 #'
-#'
-#' @param year1 The 4 digit year. A single year or lower number of the year range.
+#' @param year1 The 4 digit year, between 2017 and now. A single year or lower number of the year range.
 #' @param year2 Optional. 4 digit year, higher number of the year range.
 #'
 #' @return A [tibble()] with occurrences from GBIF for that year range of Vespa velutina in Flanders
