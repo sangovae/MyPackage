5db5c0d0b19c45e64773d646cd9f406b842fd5ac
Sanne Govaert <sangovae@Boswilg.UGent.be>
2024-02-04 19:17:21 +0100

    update tests

--- tests/testthat/test-vespadata.R
+++ tests/testthat/test-vespadata.R
@@ -1,8 +1,4 @@
 test_that("Whether vespa_data works with a single year as input", {
-
   year = sample(seq(2017,2024), 1)
-  #expect(!is.null(dim(vespa_data(year))))
-
   expect_no_condition(vespa_data(year))
-
 })
---
+++ tests/testthat/test_vespa_chart.R
@@ -0,0 +1,3 @@
+test_that("Whether there are errors", {
+  expect_no_condition(vespa_chart(vespadata_2019_2021))
+})
